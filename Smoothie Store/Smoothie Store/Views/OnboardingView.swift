9658db9a5653591065029d145ad49a0aeccb7abb
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-08-06 22:25:01 +0200

    View design changes

--- Smoothie Store/Smoothie Store/Views/OnboardingView.swift
+++ Smoothie Store/Smoothie Store/Views/OnboardingView.swift
@@ -9,9 +9,38 @@ import SwiftUI
 
 struct OnboardingView: View {
     var body: some View {
-        Text("Smoothie Town")
-            .font(.largeTitle)
-            .fontWeight(.black)
+        VStack{
+            //App Title
+            Group{
+                Image("Icon")
+                    .resizable()
+                    .frame(width: 120, height: 120)
+                    .cornerRadius(30)
+                Text("Smoothie Town")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+            }
+            
+            //Explore option
+            Group{
+                Image(systemName: "list.bullet.below.rectangle")
+                    .font(.largeTitle)
+                    .foregroundColor(.accentColor)
+                    padding(5)                
+            }
+            
+            //Sort Option
+            Group{
+                
+            }
+            
+            //Information Option
+            Group{
+                
+            }
+            Spacer()
+        }
+        
     }
 }
 

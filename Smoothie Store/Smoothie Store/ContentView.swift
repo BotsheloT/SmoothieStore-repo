ce5fda2115a4fa481d9341d0b063f0c56a522c6c
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-08-06 20:01:43 +0200

    First commit

---
+++ Smoothie Store/Smoothie Store/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Smoothie Store
+//
+//  Created by Botshelo Tlhabanyane on 2022/08/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Smoothie Store/Smoothie Store/Smoothie_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smoothie_StoreApp.swift
+//  Smoothie Store
+//
+//  Created by Botshelo Tlhabanyane on 2022/08/06.
+//
+
+import SwiftUI
+
+@main
+struct Smoothie_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

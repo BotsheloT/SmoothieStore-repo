4e1f856f9e8124389c59f0dff654205f4cc5a418
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-08-06 21:32:01 +0200

    View Name and Location change

--- Smoothie Store/Smoothie Store/Smoothie_StoreApp.swift
+++ Smoothie Store/Smoothie Store/Smoothie_StoreApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Smoothie_StoreApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            OnboardingView()
         }
     }
 }
--- Smoothie Store/Smoothie Store/Views/OnboardingView.swift
+++ Smoothie Store/Smoothie Store/Views/OnboardingView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct OnboardingView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,7 +16,7 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        OnboardingView()
             .previewInterfaceOrientation(.portrait)
     }
 }
